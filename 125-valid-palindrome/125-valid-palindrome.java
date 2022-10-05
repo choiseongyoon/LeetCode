eb6cf185f43612cacd5b2cb0b196a9ff4b07535c
choiseongyoon <minnie0401@naver.com>
2022-10-05 22:35:18 +0900

    Time: 15 ms (46.93%), Space: 44.9 MB (41.43%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String a = s.toLowerCase();
+        System.out.println(a);
+        List<Character> list = new ArrayList<>();
+        for(int i = 0;i < a.length(); i++){
+            if((a.charAt(i) >= 'a' && a.charAt(i)<='z')||a.charAt(i) >= '0' && a.charAt(i)<='9')
+                list.add( a.charAt(i));
+        }
+        // if(list.size()==0)
+        //     return true;
+        for(int i = 0;i<list.size()/2;i++){
+             if(list.get(i)!=list.get(list.size()-i-1))
+                 return false;
+        }
+        return true;
+    }
+}
